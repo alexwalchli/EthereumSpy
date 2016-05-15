f223b2403860da31a5693e02d8f36ded2c130ca8
Alex Walchli <alexwalchli@gmail.com>
2016-05-14 20:42:56 -0500

    converted sentiment service to ES6

--- services/sentimentService.js
+++ services/sentimentService.js
@@ -1,15 +1,12 @@
 var request = require('request');
 var sentiment = require('sentiment');
 
-
-function SentimentService(){
+class SentimentService{
+    getSentiment(text){
+        var score = sentiment(text).score;
     
+        return Math.tanh(score);
+    }
 }
-
-SentimentService.prototype.getSentiment = function(text){
-    var score = sentiment(text).score;
-    
-    return Math.tanh(score);
-};
 
 module.exports = SentimentService;
