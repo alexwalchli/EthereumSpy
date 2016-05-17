022b3a43d0d8bf4dd78809d636fbff7227425896
Alex Walchli <alexwalchli@gmail.com>
2016-05-17 14:21:36 -0500

    cleaned up package.json

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.get('/', function(req, res) {
     });
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-    console.log('EthereumSpy listening on: ' + port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
