3b432a55876198520071e39408c97d2da23f00a3
Alex Walchli <alexwalchli@gmail.com>
2016-05-17 14:34:37 -0500

    removing code to debug...

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log('Starting EthereumSpy...');
+
 var _ = require('lodash');
 var express = require('express');
 var exphbs  = require('express-handlebars');
@@ -5,28 +7,30 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var DataCollectionService = require('./services/dataCollectionService');
-var EthereumSpyDb = require('./ethereumSpyDb');
-var twitterConnectionInfo = {
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
-var bitcoinDataCollectionService = new DataCollectionService('BTC', process.env.ETHEREUM_SPY_DATABASE_CONN, twitterConnectionInfo);
-bitcoinDataCollectionService.scheduleDataCollection();
-var ethereumSpyDb = new EthereumSpyDb(process.env.ETHEREUM_SPY_DATABASE_CONN);
+// var DataCollectionService = require('./services/dataCollectionService');
+// var EthereumSpyDb = require('./ethereumSpyDb');
+// var twitterConnectionInfo = {
+//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// };
+// var bitcoinDataCollectionService = new DataCollectionService('BTC', process.env.ETHEREUM_SPY_DATABASE_CONN, twitterConnectionInfo);
+// bitcoinDataCollectionService.scheduleDataCollection();
+// var ethereumSpyDb = new EthereumSpyDb(process.env.ETHEREUM_SPY_DATABASE_CONN);
 
-if(process.env.NODE_ENV == 'development' && process.env.CLEAR_DB_ON_START){
-    ethereumSpyDb.clearDatabase();
-}
+// if(process.env.NODE_ENV == 'development' && process.env.CLEAR_DB_ON_START){
+//     ethereumSpyDb.clearDatabase();
+// }
 
 app.get('/', function(req, res) {
-    ethereumSpyDb.getPriceMovementPredictionResults(function(results){
-        var modelAccuracy = _.filter(results, { predictedCorrectly: true }).length / results.length;
+    // ethereumSpyDb.getPriceMovementPredictionResults(function(results){
+    //     var modelAccuracy = _.filter(results, { predictedCorrectly: true }).length / results.length;
         
-        res.render('home', { modelAccuracy: modelAccuracy, results: results });
-    });
+    //     res.render('home', { modelAccuracy: modelAccuracy, results: results });
+    // });
+    
+    res.render('home', { });
 });
 
 app.listen(process.env.PORT || 3000, function(){
